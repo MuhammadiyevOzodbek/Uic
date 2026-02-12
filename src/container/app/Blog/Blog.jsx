22bc7e6752268726e52c66b63020302ed19966bd
FazliddinNizomiddinovf <nizomiddinov20112012@gmail.com>
2026-02-12 21:02:11 +0500

    uic

--- src/container/app/Blog/Blog.jsx
+++ src/container/app/Blog/Blog.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function Blog() {
-  return (
-    <div>
-      <div>
-        <h1 className=''>BLOG</h1>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                
+                <div class="font-greyCliff relative flex items-center justify-center whitespace-nowrap border-solid stroke-[#ffffff1a] text-[56px] font-extrabold uppercase leading-[67px] sm:text-[56px] sm:leading-[67px] md:text-[100px] md:leading-[150px] lg:text-[210px] lg:leading-[252px]"><h1>Blog</h1><span class="font-greyCliff absolute left-1/2 top-1/2 flex -translate-x-2/4 translate-y-[-30%] items-center justify-center text-[40px] !font-extrabold uppercase leading-[48px] !text-white sm:text-[40px] sm:leading-[48px] md:text-[56px] md:leading-[74px] lg:text-[78px] lg:leading-[94px]">Blog</span></div>
+            </div>
+        </div>
+    )
 }
