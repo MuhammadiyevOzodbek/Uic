9e06bedfb09995b9f5b421e336fd0e1204f91ece
FazliddinNizomiddinovf <nizomiddinov20112012@gmail.com>
2026-02-12 17:55:38 +0500

    uic bate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Blog from "./container/Blog/Blog"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Blog/>
     </>
   )
 }
---
+++ src/container/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Blog() {
+  return (
+    <div>
+      <div>
+        <h1 className=''>BLOG</h1>
+      </div>
+    </div>
+  )
+}
