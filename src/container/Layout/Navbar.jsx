ad73ffe40d8452e5ce912592224eb246db8f550e
Mashhurbek <ergashevmashurbek08@gmail.com>
2026-02-12 22:13:35 +0500

    Satni o'zgartirdim

--- src/container/Layout/Navbar.jsx
+++ src/container/Layout/Navbar.jsx
@@ -1,21 +1,75 @@
-import uicLogo from "../../../public/layoutImg/UIC logo-1.svg"
+import { useState } from "react";
+import uicLogo from "../../../public/layoutImg/UIC logo-1.svg";
 
-function NavbarLayout() {
-  return(
-    <nav className="flex justify-between items-center my-3">
-      <img src={uicLogo} alt="" />
-      <article className="flex justify-between items-center w-138">
-        <ul className="flex justify-between w-128 text-gray-500 duration-700">
-          <li className="hover:text-white">ABOUT US</li>
-          <li className="hover:text-white">SERVICES</li>
-          <li className="hover:text-white">PORTFOLIO</li>
-          <li className="hover:text-white">CAREER</li>
-          <li className="hover:text-white">BLOG</li>
-        </ul>
-        <p className="text-gray-400 text-2xl font-bold">+998 71 <span className="text-white">200 70 07</span></p>
-      </article>
+function Navbar() {
+  const [open, setOpen] = useState(false);
+  const [language, setLanguage] = useState("EN");
+
+  return (
+    <nav className="w-full absolute top-0 left-0 z-50">
+      <div className="max-w-[1200px] mx-auto flex justify-between items-center pt-[20px] px-4">
+
+        <div className="flex items-center gap-2">
+          <img src={uicLogo} alt="UIC" className="w-[100px]" />
+          <span className="text-white text-sm">
+          </span>
+        </div>
+
+        <div className="flex items-center gap-20 relative ">
+
+          <ul
+            className={`flex items-center gap-8 text-gray-300 text-sm tracking-wide transition-all duration-500
+            ${open ? "-translate-x-20 opacity-0" : "translate-x-0 opacity-100"}`}
+          >
+            <li className="hover:text-white cursor-pointer font-normal text-[16px]">ABOUT US</li>
+            <li className="hover:text-white cursor-pointer font-normal text-[16px]">SERVICES</li>
+            <li className="hover:text-white cursor-pointer font-normal text-[16px]">PORTFOLIO</li>
+            <li className="hover:text-white cursor-pointer font-normal text-[16px]">CAREER</li>
+            <li className="hover:text-white cursor-pointer font-normal text-[16px]">BLOG</li>
+          </ul>
+
+          <p
+            className={`text-[#9aafb2] text-[22px] font-semibold transition-all duration-500
+            ${open ? "-translate-x-20 opacity-0" : "translate-x-0 opacity-100"}`}
+          >
+            +998 71 <span className="text-white">200 70 07</span>
+          </p>
+
+          <div
+            className="relative"
+            onMouseEnter={() => setOpen(true)}
+            onMouseLeave={() => setOpen(false)}
+          >
+            <div
+              className={`flex justify-center items-center gap-3 p-2 rounded-[10px] border border-gray-500 bg-white/5 backdrop-blur-md cursor-pointer transition-all duration-500 overflow-hidden ${open ? "w-[260px]" : "w-[110px]"}`}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className=" w-6" viewBox="0 0 640 640"><path fill="#b8b8b8" d="M119.7 263.7L150.6 294.6C156.6 300.6 164.7 304 173.2 304L194.7 304C203.2 304 211.3 307.4 217.3 313.4L246.6 342.7C252.6 348.7 256 356.8 256 365.3L256 402.8C256 411.3 259.4 419.4 265.4 425.4L278.7 438.7C284.7 444.7 288.1 452.8 288.1 461.3L288.1 480C288.1 497.7 302.4 512 320.1 512C337.8 512 352.1 497.7 352.1 480L352.1 477.3C352.1 468.8 355.5 460.7 361.5 454.7L406.8 409.4C412.8 403.4 416.2 395.3 416.2 386.8L416.2 352.1C416.2 334.4 401.9 320.1 384.2 320.1L301.5 320.1C293 320.1 284.9 316.7 278.9 310.7L262.9 294.7C258.7 290.5 256.3 284.7 256.3 278.7C256.3 266.2 266.4 256.1 278.9 256.1L313.6 256.1C326.1 256.1 336.2 246 336.2 233.5C336.2 227.5 333.8 221.7 329.6 217.5L309.9 197.8C306 194 304 189.1 304 184C304 178.9 306 174 309.7 170.3L327 153C332.8 147.2 336.1 139.3 336.1 131.1C336.1 123.9 333.7 117.4 329.7 112.2C326.5 112.1 323.3 112 320.1 112C224.7 112 144.4 176.2 119.8 263.7zM528 320C528 285.4 519.6 252.8 504.6 224.2C498.2 225.1 491.9 228.1 486.7 233.3L473.3 246.7C467.3 252.7 463.9 260.8 463.9 269.3L463.9 304C463.9 321.7 478.2 336 495.9 336L520 336C522.5 336 525 335.7 527.3 335.2C527.7 330.2 527.8 325.1 527.8 320zM64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320z" /></svg>
+              {!open ? (
+                <span className="text-gray-300 font-semibold">
+                  {language}
+                </span>
+              ) : (
+                <div className="flex items-center pr-4 gap-4 text-[17px] whitespace-nowrap">
+                  <svg xmlns="http://www.w3.org/2000/svg" className=" w-6" viewBox="0 0 640 640"><path fill="#b8b8b8" d="M119.7 263.7L150.6 294.6C156.6 300.6 164.7 304 173.2 304L194.7 304C203.2 304 211.3 307.4 217.3 313.4L246.6 342.7C252.6 348.7 256 356.8 256 365.3L256 402.8C256 411.3 259.4 419.4 265.4 425.4L278.7 438.7C284.7 444.7 288.1 452.8 288.1 461.3L288.1 480C288.1 497.7 302.4 512 320.1 512C337.8 512 352.1 497.7 352.1 480L352.1 477.3C352.1 468.8 355.5 460.7 361.5 454.7L406.8 409.4C412.8 403.4 416.2 395.3 416.2 386.8L416.2 352.1C416.2 334.4 401.9 320.1 384.2 320.1L301.5 320.1C293 320.1 284.9 316.7 278.9 310.7L262.9 294.7C258.7 290.5 256.3 284.7 256.3 278.7C256.3 266.2 266.4 256.1 278.9 256.1L313.6 256.1C326.1 256.1 336.2 246 336.2 233.5C336.2 227.5 333.8 221.7 329.6 217.5L309.9 197.8C306 194 304 189.1 304 184C304 178.9 306 174 309.7 170.3L327 153C332.8 147.2 336.1 139.3 336.1 131.1C336.1 123.9 333.7 117.4 329.7 112.2C326.5 112.1 323.3 112 320.1 112C224.7 112 144.4 176.2 119.8 263.7zM528 320C528 285.4 519.6 252.8 504.6 224.2C498.2 225.1 491.9 228.1 486.7 233.3L473.3 246.7C467.3 252.7 463.9 260.8 463.9 269.3L463.9 304C463.9 321.7 478.2 336 495.9 336L520 336C522.5 336 525 335.7 527.3 335.2C527.7 330.2 527.8 325.1 527.8 320zM64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320z" /></svg>
+                  <span className="text-gray-400">Language:</span>
+
+                  {["EN", "RU", "UZ"].map((lang) => (
+                    <span
+                      key={lang}
+                      onClick={() => setLanguage(lang)}
+                      className={`cursor-pointer font-semibold transition ${language === lang ? "text-white" : "text-gray-400 hover:text-white"}`}>
+                      {lang}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+
+        </div>
+      </div>
     </nav>
   )
 }
 
-export default NavbarLayout
+export default Navbar
