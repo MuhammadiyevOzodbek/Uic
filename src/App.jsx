f59d1888dde4fd4df426abf3bd38d7ba51b7dd6e
MuhammadiyevOzodbek <ozodbekmuhammadiyev2001@gmail.com>
2026-02-12 20:05:54 +0500

    uic beta

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Blog from "./container/Blog/Blog"
-
 function App() {
 
   return (
     <>
-      <Blog/>
     </>
   )
 }
 
-export default App
+export  default App
--- src/container/Layout/Navbar.jsx
+++ src/container/Layout/Navbar.jsx
@@ -1,21 +1,8 @@
-import React from 'react'
-import img1 from '../../public/layoutImg/UIC logo-1.svg'
-
-export default function Navbar() {
-  return (
+function NavbarLayout() {
+  return(
     <div>
-      <nav>
-        <div>
-            <img src={img1} alt="" />
-        </div>
-        <ul>
-            <li>About us</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Career</li>
-            <li>Blog</li>
-        </ul>
-      </nav>
     </div>
   )
 }
+
+export default NavbarLayout
