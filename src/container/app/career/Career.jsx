243763cc9020107ac7a486c334526ac8e14901c7
MuhammadiyevOzodbek <ozodbekmuhammadiyev2001@gmail.com>
2026-02-13 20:44:09 +0500

    uic beta

--- src/container/app/career/Career.jsx
+++ src/container/app/career/Career.jsx
@@ -2,9 +2,8 @@
 function Career () {
     return(
         <div className="max-w-[1200px] m-[auto] ">
-            <div className="bg-white">
-                <h1 className="">CAREER <span>CAREER</span></h1>
-            </div>
+            
+                <div class="text-transparent relative flex items-center justify-center whitespace-nowrap border-solid border-inherit text-[56px] font-extrabold uppercase leading-[67px] sm:text-[56px] sm:leading-[67px] md:text-[100px] md:leading-[150px] lg:text-[210px] lg:leading-[252px]"><h1>Career</h1><span class="font-greyCliff absolute left-1/2 top-1/2 flex -translate-x-2/4 translate-y-[-30%] items-center justify-center text-[40px] !font-extrabold uppercase leading-[48px] !text-white sm:text-[40px] sm:leading-[48px] md:text-[56px] md:leading-[74px] lg:text-[78px] lg:leading-[94px]">Career</span></div>
         </div>
     );
 }
