b0340d5934ab036478a190f843f93c9ea54e6d6b
FazliddinNizomiddinovf <nizomiddinov20112012@gmail.com>
2026-02-15 11:06:04 +0500

    Uic

--- src/container/app/Blog/Blog.jsx
+++ src/container/app/Blog/Blog.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Blog() {
     return (
-        <div>
-            <div>
-                <div class="font-greyCliff relative flex items-center justify-center whitespace-nowrap border-solid stroke-[#ffffff1a] text-[56px] font-extrabold uppercase leading-[67px] sm:text-[56px] sm:leading-[67px] md:text-[100px] md:leading-[150px] lg:text-[210px] lg:leading-[252px]"><h1>Blog</h1><span class="font-greyCliff absolute left-1/2 top-1/2 flex -translate-x-2/4 translate-y-[-30%] items-center justify-center text-[40px] !font-extrabold uppercase leading-[48px] !text-white sm:text-[40px] sm:leading-[48px] md:text-[56px] md:leading-[74px] lg:text-[78px] lg:leading-[94px]">Blog</span></div>
+        <div className="w-[100hv] m-[auto] ">
+            <div className="h-[500px] flex justify-center items-center">
+                <h1 className="text-[230px] font-bold text-transparent text-stroke">BLOG<span className="text-[85px] text-stroke font-bold absolute z-10 top-[290px] right-[610px] text-white text-stroke-none">Blog</span></h1>
             </div>
         </div>
     )
