eabfa68ea7ecaf7e4fa09512b4de183f7727e510
MuhammadiyevOzodbek <ozodbekmuhammadiyev2001@gmail.com>
2026-02-15 12:05:44 +0500

    uic beta

--- src/container/app/career/Career.jsx
+++ src/container/app/career/Career.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 
 function Career () {
     return(
-        <div className="w-[100hv] m-[auto] ">
+        <div className="w-[100hv] m-[auto]">
             <div className="h-[500px] flex justify-center items-center">
                 <h1 className="text-[230px] font-bold text-transparent text-stroke">CAREER<span className="text-[85px] text-stroke font-bold absolute z-10 top-[290px] right-[610px] text-white text-stroke-none">CAREER</span></h1>
             </div>
