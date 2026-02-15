1473ea2860e1fb43e3330985455d6449e861951f
MuhammadiyevOzodbek <ozodbekmuhammadiyev2001@gmail.com>
2026-02-15 11:16:18 +0500

    uic beta

--- src/container/app/Blog/Blog.jsx
+++ src/container/app/Blog/Blog.jsx
@@ -4,7 +4,7 @@ export default function Blog() {
     return (
         <div className="w-[100hv] m-[auto] ">
             <div className="h-[500px] flex justify-center items-center">
-                <h1 className="text-[230px] font-bold text-transparent text-stroke">BLOG<span className="text-[85px] text-stroke font-bold absolute z-10 top-[290px] right-[610px] text-white text-stroke-none">Blog</span></h1>
+                <h1 className="text-[230px] font-bold text-transparent text-stroke">BLOG<span className="text-[85px] text-stroke font-bold absolute z-10 top-[290px] right-[700px] text-white text-stroke-none">Blog</span></h1>
             </div>
         </div>
     )
